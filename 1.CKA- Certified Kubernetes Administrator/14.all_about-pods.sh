1377497c1c1717801f111b4c7c26deb95c11919b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2024-08-30 06:19:23 +0530

    curl with header i.e. curl -v -H "Accept: text/html" localhost:8888

--- 1.CKA- Certified Kubernetes Administrator/14.all_about-pods.sh
+++ 1.CKA- Certified Kubernetes Administrator/14.all_about-pods.sh
@@ -7,3 +7,98 @@ pod/nginx created
 bharathkumardasaraju@Downloads$
 
 
+bharathkumardasaraju@~$ kubectl get pods -o wide
+NAME    READY   STATUS    RESTARTS   AGE     IP            NODE       NOMINATED NODE   READINESS GATES
+nginx   1/1     Running   0          6m16s   10.244.0.17   minikube   <none>           <none>
+bharathkumardasaraju@~$
+
+bharathkumardasaraju@~$ kubectl get pods -o wide -n default
+NAME    READY   STATUS    RESTARTS   AGE     IP            NODE       NOMINATED NODE   READINESS GATES
+nginx   1/1     Running   0          6m27s   10.244.0.17   minikube   <none>           <none>
+bharathkumardasaraju@~$
+
+
+bharathkumardasaraju@Downloads$ kubectl port-forward pod/nginx 8888:80
+Forwarding from 127.0.0.1:8888 -> 80
+Forwarding from [::1]:8888 -> 80
+Handling connection for 8888
+Handling connection for 8888
+Handling connection for 8888
+...
+...
+bharathkumardasaraju@Downloads$
+
+
+bharathkumardasaraju@~$ curl localhost:8888
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+bharathkumardasaraju@~$
+
+
+
+
+bharathkumardasaraju@~$ curl -v -H "Accept: text/html" localhost:8888
+*   Trying [::1]:8888...
+* Connected to localhost (::1) port 8888
+> GET / HTTP/1.1
+> Host: localhost:8888
+> User-Agent: curl/8.4.0
+> Accept: text/html
+>
+< HTTP/1.1 200 OK
+< Server: nginx/1.27.1
+< Date: Fri, 30 Aug 2024 00:48:08 GMT
+< Content-Type: text/html
+< Content-Length: 615
+< Last-Modified: Mon, 12 Aug 2024 14:21:01 GMT
+< Connection: keep-alive
+< ETag: "66ba1a4d-267"
+< Accept-Ranges: bytes
+<
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+* Connection #0 to host localhost left intact
+bharathkumardasaraju@~$
+
