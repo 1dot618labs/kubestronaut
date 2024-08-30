01b08dada5a5a3465a94d633f63875c1ea2f63c7
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2024-08-30 06:09:53 +0530

    all about pods - smallest object in k8s

---
+++ 1.CKA- Certified Kubernetes Administrator/14.all_about-pods.sh
@@ -0,0 +1,9 @@
+bharathkumardasaraju@Downloads$ kubectl run nginx
+error: required flag(s) "image" not set
+bharathkumardasaraju@Downloads$
+
+bharathkumardasaraju@Downloads$ kubectl run nginx --image=nginx
+pod/nginx created
+bharathkumardasaraju@Downloads$
+
+
